cf182a64b7c8cc6fa044a4c6b26d8b8a82defd51
shane tully <esc201@gmail.com>
2013-01-12 21:47:13 -0500

    changed licences to LGPL

--- README.md
+++ README.md
@@ -27,8 +27,21 @@ from its author. However, bugs and feature requests may be submitted to the GitH
 linked to above.
 
 
-## Legal
+## License
+Copyright (C) 2013 Shane Tully
 
-This program is open source software. It is free to distribute, modify, and use
-with the exception of it being made closed source or sold for commercial purposes
-without the consent of the author.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
--- google-music-playlist-sync.py
+++ google-music-playlist-sync.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
-# Shane Tully
-# 
+# Shane Tully (shane@shanetully.com)
+# shanetully.com
+
 # GitHub repo: https://github.com/shanet/Google-Music-Playlist-Sync
-#
-# This program is open source software. It is free to distribute, modify, and use
-# with the exception of it being made closed source and sold for commercial purposes
-# without the consent of the author.
-#
 # Makes use of the Unofficial Google Music API by Simon Weber
 # https://github.com/simon-weber/Unofficial-Google-Music-API
+
+# Copyright (C) 2013 Shane Tully
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 from os import path
