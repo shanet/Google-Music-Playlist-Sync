a1a82a253270be9722eff11181d176a4dbbc2717
shane tully <esc201@gmail.com>
2013-02-08 04:05:19 -0500

    added shebang, changed version and license

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
 setup(
     name='Google-Music-Playlist-Sync',
-    version='0.1.0',
+    version='1.0.0',
     author='Shane Tully',
     author_email='shane@shanetully.com',
     scripts=['google-music-playlist-sync.py'],
     url='https://github.com/shanet/Google-Music-Playlist-Sync',
-    license='COPYING',
+    license='LGPL3',
     description='A quick and dirty Python script to sync local XSPF playlists to Google Music',
     install_requires=[
         'gmusicapi >= 2013.01.05'
